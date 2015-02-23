bca7a7dc8dbae34607f50113898fe99fc208a20b
Francisco Demartino <demartino.francisco@gmail.com>
2015-02-23 01:06:54 -0300

    join cpanm lines

--- provision.sh
+++ provision.sh
@@ -2,11 +2,7 @@ sudo apt-get update
 
 sudo apt-get install -y apache2 build-essential cpanminus libgd2-xpm-dev libxml2-dev weblint-perl
 
-sudo cpanm GD
-sudo cpanm Digest::SHA1
-sudo cpanm XML::LibXML
-sudo cpanm Crypt::OpenSSL::AES
-sudo cpanm Crypt::CBC
+sudo cpanm GD Digest::SHA1 XML::LibXML Crypt::OpenSSL::AES Crypt::CBC
 
 SRCDIR="/vagrant/src"
 DATADIR="/var/lib/hackdiet"
